66e1bf7995cf135347318f0b872a8d69625b41c9
Garima <saxenagarima2953@gmail.com>
2024-10-26 22:32:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const carousel = document.querySelector(".carousel");
 const slideContainer = document.querySelector(".slide-wrapper");
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
+const progressBar = document.querySelector("#progress");
 
 let currSlide = 0;
 let totalSlides = 10;
 let slides = 0;
+let intervalDuration = 4000;
+let progressPercentage = 0;
+let progressTime;
 
 
 function pushSlide (){
@@ -22,21 +26,36 @@ function pushSlide (){
     console.log(slides);
 }
 
-
 function setSlides(){
     slides.forEach((slide, index) => {
         slide.style.transform = `translateX(${(index - currSlide) * 100}%)`;
-    })
+    });
 }
 
 function prevSlide(){
     currSlide = (currSlide - 1 + totalSlides) % totalSlides;
+    resetProgress();
     setSlides();
 }
 
 function nextSlide(){
     currSlide = (currSlide + 1) % totalSlides;
+    resetProgress();
     setSlides();
+}
+
+function setProgress(){
+    progressPercentage++;
+    if (progressPercentage >= 100) {
+        progressPercentage = 0; 
+        nextSlide(); 
+    }
+    progressBar.style.width = `${progressPercentage}%`;
+}
+
+function resetProgress(){
+    progressPercentage = 0; 
+    progressTime = setInterval(setProgress, intervalDuration / 100);
 }
 
 prev.addEventListener("click", prevSlide);
@@ -45,6 +64,7 @@ next.addEventListener("click", nextSlide);
 function main(){
     pushSlide();
     setSlides();
+    resetProgress();
 }
 
 main();
